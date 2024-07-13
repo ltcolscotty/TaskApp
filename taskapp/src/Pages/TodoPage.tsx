1e17851a5c484c7521ae169ef57a507c72850a84
MidnightChaos <aidan.yung@gmail.com>
2024-07-13 00:34:01 -0700

    rebuilt with yarn

---
+++ taskapp/src/Pages/TodoPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "../Components/Card/card";
+
+const TodoPage = ()=> {
+    return (
+        <>
+            <Card/>
+        </>
+    )
+}
+
+export default TodoPage;
