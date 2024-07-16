a472a00d11d12494144d585413dfabefec1ff442
MidnightChaos <aidan.yung@gmail.com>
2024-07-16 02:10:38 -0700

    new changes

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-'''
+"""
 USE:
 $env:FLASK_APP = "api.py"
 flask run
@@ -6,28 +6,35 @@ flask run
 then you can use:
 yarn start
 
-'''
+"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(os.getcwd(), "tasks.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    os.getcwd(), "tasks.db"
+)
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
 
     def __str__(self):
-        return f'{self.id} {self.content}'
+        return f"{self.id} {self.content}"
 
-@app.route('/api', methods=['GET'])
+
+def todo_serializer(todo):
+    return {"id": todo.id, "content": todo.content}
+
+
+@app.route("/api", methods=["GET"])
 def index():
-    return {
-        'name': ['apple, orange']
-    }
+    return jsonify([*map(todo_serializer, Todo.query.all())])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- scriptun.py
+++ scriptun.py
@@ -1,4 +1,13 @@
 from api import app, db
+from api import Todo
+
 
 with app.app_context():
     db.create_all()
+    todo = Todo(content='Test task')
+    db.session.add(todo)
+    db.session.commit()
+
+    todo2 = Todo(content='test task 2')
+    db.session.add(todo2)
+    db.session.commit()
